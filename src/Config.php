74afd227c0b9b647d699a35379e2df8281c5bebd
Grant Scoular <gscoular@vendasta.com>
2019-10-03 10:50:08 -0600

    Config file

--- src/Config.php
+++ src/Config.php
@@ -4,15 +4,15 @@ class Config
 {
     const ENVIRONMENT_PARAMS = [
         "PROD" => [
-            'host' => 'accounts-api-prod.apigateway.co:443',
-            'scope' => 'https://accounts-api-prod.apigateway.co',
-            'url' => 'https://accounts-api-prod.apigateway.co',
+            'host' => 'accounts-prod.apigateway.co:443',
+            'scope' => 'https://accounts-prod.apigateway.co',
+            'url' => 'https://accounts-prod.apigateway.co',
             'secure' => true,
         ],
         "DEMO" => [
-            'host' => 'accounts-api-demo.apigateway.co:443',
-            'scope' => 'https://accounts-api-demo.apigateway.co',
-            'url' => 'https://accounts-api-demo.apigateway.co',
+            'host' => 'accounts-demo.apigateway.co:443',
+            'scope' => 'https://accounts-demo.apigateway.co',
+            'url' => 'https://accounts-demo.apigateway.co',
             'secure' => true,
         ]
     ];
